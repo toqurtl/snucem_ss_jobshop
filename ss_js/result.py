ddf08d1e28efe71e0fb064a7147b26a53670117e
toqurtl <yoon92411@naver.com>
2021-10-14 16:16:32 +0900

    api 정리

--- ss_js/result.py
+++ ss_js/result.py
@@ -5,44 +5,48 @@ import datetime
 import plotly as py
 import plotly.figure_factory as ff
 import pandas as pd
-import threading
-from multiprocessing import Process
-import pickle
-
-def print_value(schedule, solver):
-    print('fuck')
-    for task in schedule.task_dict.values():            
-        start_value = solver.Value(task.start_var)
-        print(start_value)
-
 
 
 class SolutionPrinter(cp_model.CpSolverSolutionCallback):
     """Print intermediate solutions."""
 
-    def __init__(self, schedule: Schedule, result):
+    def __init__(self, schedule: Schedule, target_obj = 2000, monitoring_cycle = 10):
         cp_model.CpSolverSolutionCallback.__init__(self)
         self.__solution_count = 0
         self.schedule = schedule
-        self.result = result
+        self.target_obj = target_obj  
+        self.monitoring_cycle = monitoring_cycle
+        # results
+        self.num_labor_dict = {} # t:{labor_type_id: num_labor}
+        self.task_dict = {} # t: task_id, alter_dict
+        self.result_data = []
+        self.figure_data = []
+        self.labor_dict = {} # type_id: [1,2,3,4,5]   
 
     def on_solution_callback(self):
         """Called at each new solution."""
         print('Solution %i, time = %f s, objective = %i' %
               (self.__solution_count, self.WallTime(), self.ObjectiveValue()))                   
         
-        if self.__solution_count % 10 == 1:
-            print(self.optimal_schedule())
-
-        if self.ObjectiveValue() < 1800:
+        # 확인하고 싶은 데이터 저장
+        if self.__solution_count % self.monitoring_cycle == 0:                        
+            self.set_task_dict()
+            # print(self.get_work_data_of_solution())
+            # for time, task in self.task_dict.items(): 
+            #     print(time, task)
+            # for time, labor_info in self.num_labor_dict.items():
+            #     print(time, labor_info)
+            
+        if self.ObjectiveValue() < self.target_obj:
             self.StopSearch()
+            
         self.__solution_count += 1
     
-    def check_procedure(self):
-        dt = datetime.datetime
-        time_delta = datetime.timedelta
-        today = dt.today()
-        start_date = dt(today.year,today.month,today.day)
+    def get_work_data_of_solution(self):
+        # dt = datetime.datetime
+        # time_delta = datetime.timedelta
+        # today = dt.today()
+        # start_date = dt(today.year,today.month,today.day)
         result_data = []
         for task in self.schedule.task_dict.values():            
             start_value = self.Value(task.start_var)                
@@ -52,77 +56,22 @@ class SolutionPrinter(cp_model.CpSolverSolutionCallback):
                     duration = alter.duration
                     selected = alter.id
                     labor_info = alter.info[Params.REQUIRED_LABOR]           
-            result_data.append([task.id, selected, start_value, end_value, labor_info])
-        
+            result_data.append([task.id, start_value, end_value, labor_info])        
         return result_data
-
-
-class OptimalResult(object):
-    def __init__(self, schedule: Schedule):
-        self.solver = cp_model.CpSolver()                
-        self.schedule = schedule
-        self.status = self.solver.SolveWithSolutionCallback(self.schedule, SolutionPrinter(self.schedule, self))        
-        print('ssibalnom')
-        # results
-        self.num_labor_dict = {} # t:{labor_type_id: num_labor}
-        self.task_dict = {} # t: task_id, alter_dict
-        self.result_data = []
-        self.figure_data = []
-
-        self.labor_dict = {} # type_id: [1,2,3,4,5]        
-        self.optimal_schedule()
-        self.set_task_dict()        
-        return
-
-    @property # 최적화 성공 여부
-    def is_optimal(self):
-        return self.status == cp_model.OPTIMAL
-
-    @property
-    def optimal_makespan(self):
-        return int(self.solver.ObjectiveValue())
-
-    
-
-    def optimal_schedule(self):
-        dt = datetime.datetime
-        time_delta = datetime.timedelta
-        today = dt.today()
-        start_date = dt(today.year,today.month,today.day)
-        # for zone in self.schedule.zone_dict.values():
-            # for task in zone.task_list:
-        for task in self.schedule.task_dict.values():            
-            start_value = self.solver.Value(task.start_var)                
-            end_value = self.solver.Value(task.end_var)                
-            for alter in task.alt_dict.values():
-                if self.solver.Value(alter.presence_var):
-                    duration = alter.duration
-                    selected = alter.id
-                    labor_info = alter.info[Params.REQUIRED_LABOR]
-            self.figure_data.append(dict(
-                Task=task.id,
-                Start= start_date + time_delta(0, start_value),
-                Finish=start_date + time_delta(0, end_value),
-                # complete=zone.id,
-                Resource=task.space_id_list[0]
-            ))
-            
-            self.result_data.append([task.id, selected, start_value, end_value, labor_info])
-        
-        return
 
     def create_gantt(self, data_path):
         df = pd.DataFrame(self.figure_data)
         pyplt = py.offline.plot
-        # colors = {
-        #     'cme4_1': 'rgb(255,0,0)',
-        #     'cme4_2': 'rgb(0, 255, 0)'
-        # }
+        colors = {
+            'cme4': 'rgb(255,0,0)',
+            'cep13': 'rgb(0,255,0)',
+            'cme5': 'rgb(0,0,255)',                 
+        }
         fig = ff.create_gantt(
             df,
-            # colors= colors,
+            colors= colors,
             index_col='Resource',
-            # show_colorbar = True,
+            show_colorbar = True,
             group_tasks = True
         )
         pyplt(fig, filename=data_path, auto_open=False)
@@ -130,7 +79,7 @@ class OptimalResult(object):
     
     # 시간별 task
     def set_task_dict(self):
-        makespan = self.solver.ObjectiveValue()        
+        makespan = self.ObjectiveValue()        
         for time in range(0, int(makespan)):
             self.task_dict[time] = {}
             self.num_labor_dict[time] = {}
@@ -138,6 +87,15 @@ class OptimalResult(object):
                 self.put_alter_of_task_at_time(task, time)
         return
                 
+    def put_alter_of_task_at_time(self, task, time):        
+        for alter in task.alt_dict.values():
+            if self.Value(alter.presence_var):
+                start_value, end_value = self.Value(task.start_var), self.Value(task.end_var)
+                if start_value <= time < end_value:
+                    self.task_dict[time][task.id] = (task.space_id, alter)
+                    self.put_num_labor_dict_of_alter_at_time(time, alter)
+        return
+
     def put_num_labor_dict_of_alter_at_time(self, time, alter):
         for labor_type_id in alter.labor_type_id_list():
             if labor_type_id in self.num_labor_dict[time].keys():
@@ -145,17 +103,6 @@ class OptimalResult(object):
             else:
                 self.num_labor_dict[time][labor_type_id] = alter.num_labor_type(labor_type_id)
         return
-
-
-    def put_alter_of_task_at_time(self, task, time):        
-        for alter in task.alt_dict.values():
-            if self.solver.Value(alter.presence_var):
-                start_value, end_value = self.solver.Value(task.start_var), self.solver.Value(task.end_var)
-                if start_value <= time < end_value:
-                    self.task_dict[time] = (task.id, alter.id)
-                    self.put_num_labor_dict_of_alter_at_time(time, alter)
-        return
-
 
     # 시간별 labor 숫자 관련
     def set_labor_allocation(self):
@@ -172,8 +119,5 @@ class OptimalResult(object):
                 num_labor = labor_info[labor_type_id]
                 num_labor_type_id_at_t_list.append(num_labor)        
         return max(num_labor_type_id_at_t_list)
-
-
-
 
 
