6e131ee4eae727de65a4e546f1e9cc0b6d5c33dc
toqurtl <yoon92411@snu.ac.kr>
2021-10-17 12:27:07 +0900

    labor chart 추가

--- app_makeresult.py
+++ app_makeresult.py
@@ -14,20 +14,31 @@ file_path = save_dir+"/input/input.xlsx"
 generated_path = save_dir+"/input/input.json"
 
 
-
 with open(generated_path, 'r', encoding="utf-8") as f:
     json_data = json.load(f)
 
 schedule = Schedule(json_data)
 
+
 ## chart
 
-chart.create_gantt(save_dir+"/output/solution"+solution_ctn+"_tasklist.json", save_dir+"/result/solution"+solution_ctn+"_", schedule)
+chart.create_gantt(save_dir+"/output/solution"+solution_ctn+"_tasklist.json", 
+    save_dir+"/result/solution"+solution_ctn+"_", schedule
+)
+print("gantt chart generated")
 # excel
 json_tasklist_path = save_dir+"/output/solution"+solution_ctn+"_tasklist.json"
-excel_tasklist_path = save_dir+"/result/solution"+solution_ctn+"_tasklist.xlsx"
+excel_tasklist_path = save_dir+"/result/solution"+solution_ctn+"_tasklist"
 chart.task_list_to_excel(json_tasklist_path, excel_tasklist_path)
+print("task_list_excel file generated")
 
 json_labortime_path = save_dir+"/output/solution"+solution_ctn+"_labortime.json"
-excel_labortime_path = save_dir+"/result/solution"+solution_ctn+"_labortime.xlsx"
+excel_labortime_path = save_dir+"/result/solution"+solution_ctn+"_labortime"
 chart.labor_time_to_excel(json_labortime_path, excel_labortime_path)
+print("labor_time excel file generated")
+
+chart.create_labor_chart(
+    save_dir+"/output/solution"+solution_ctn+"_labortime.json",
+    save_dir+"/result/solution"+solution_ctn+"_"
+)
+print("labor chart generated")
--- ss_js/optimize/chart.py
+++ ss_js/optimize/chart.py
@@ -6,6 +6,8 @@ from ss_js import utils
 from ss_js.schedule.schedule import Schedule
 import json
 import openpyxl
+import plotly.graph_objects as go
+import math
 
 
 def task_list_to_excel(data_path, result_path):
@@ -13,17 +15,25 @@ def task_list_to_excel(data_path, result_path):
         task_list_data = json.load(f)
     col_names = ["task_id", "workpackage_id", "section", "space_id", "start_value", "end_value","duration", "labor_type", "num_labor", "productivity", "quantity"]
     wb = openpyxl.Workbook()
-    wb.create_sheet('task_list')    
-    task_list_sheet = wb['task_list']    
+    wb.create_sheet('task_list')
+    wb.create_sheet('task_list(day)')
+    task_list_sheet = wb['task_list']
+    task_list_day_sheet = wb['task_list(day)']   
 
     for seq, name in enumerate(col_names):
         task_list_sheet.cell(row=1, column=seq+1, value=name)
+        task_list_day_sheet.cell(row=1, column=seq+1, value=name)
     
     for idx, data in enumerate(task_list_data):
         for seq, name in enumerate(col_names):            
             task_list_sheet.cell(row=idx+2, column=seq+1, value=data[name])
+            if name in ["start_value", "end_value","duration"]:
+                day_data = math.ceil(data[name]/480)
+                task_list_day_sheet.cell(row=idx+2, column=seq+1, value=day_data)
+            else:
+                task_list_day_sheet.cell(row=idx+2, column=seq+1, value=data[name])
     
-    wb.save(result_path)
+    wb.save(result_path+".xlsx")
     wb.close()
     return
 
@@ -36,34 +46,67 @@ def get_labor_time_col_list(labor_data):
                 labor_list.append(labor_type)
     return labor_list
 
-def labor_time_to_excel(data_path, result_path):
+def labor_time_to_json(data_path, result_path):
     with open(data_path, 'r', encoding="utf-8") as f:
+        labor_time_data = json.load(f)
+    
+    new_json_data = {}
+    labor_time_col_list = get_labor_time_col_list(labor_time_data)    
+    
+    for seq, name in enumerate(labor_time_col_list):
+        new_json_data[name] = []
+        
+    idx = 0    
+    for time, labor_info in labor_time_data.items():
+        new_json_data["time"].append(int(time))
+        for seq, labor_type_id in enumerate(labor_time_col_list):
+            if seq == 0:
+                pass
+            else: 
+                if labor_type_id in labor_info.keys():
+                    num_labor = labor_info[labor_type_id]                    
+                    new_json_data[labor_type_id].append(num_labor)
+                else:                    
+                    new_json_data[labor_type_id].append(0)
+        idx += 1    
+    with open(result_path+".json", 'w', encoding="utf-8") as f:
+         json.dump(new_json_data, f, ensure_ascii=False)
+    return
+
+def labor_time_to_excel(data_path, result_path):    
+    labor_time_to_json(data_path, result_path)
+    with open(result_path+".json", 'r', encoding="utf-8") as f:
         labor_time_data = json.load(f)
 
     wb = openpyxl.Workbook()    
     wb.create_sheet('labor_time')        
+    wb.create_sheet('labor_time_day')   
     labor_time_sheet = wb['labor_time']
+    labor_time_day_sheet = wb['labor_time_day']
 
-    labor_time_col_list = get_labor_time_col_list(labor_time_data)
+    for idx, col_name in enumerate(labor_time_data.keys()):
+        labor_time_sheet.cell(row=1, column=idx+1, value=col_name)
+        labor_time_day_sheet.cell(row=1, column=idx+1, value=col_name)
+        
+    col_idx = 1    
+    for col_name, row_list in labor_time_data.items():
+        day_data = []        
+        for row_idx, row_value in enumerate(row_list):            
+            labor_time_sheet.cell(row=row_idx+2, column=col_idx, value=row_value)
+            day_data.append(row_value)            
+            if col_name == "time":
+                if row_idx % 480 == 479:
+                   labor_time_day_sheet.cell(row=(row_idx//480)+2, column=1, value=row_idx//480) 
+            else:                
+                if row_idx % 480 == 479:
+                    day = row_idx / 480
+                    day += 1
+                    num_labor = max(day_data)
+                    labor_time_day_sheet.cell(row=(row_idx//480)+2, column=col_idx, value=num_labor)                
+        col_idx += 1
 
-    for seq, name in enumerate(labor_time_col_list):
-        labor_time_sheet.cell(row=1, column=seq+1, value=name)
-    
-    idx = 0    
-    for time, labor_info in labor_time_data.items():
-        for seq, labor_type_id in enumerate(labor_time_col_list):
-            if seq == 0:
-                labor_time_sheet.cell(row=idx+2, column=1, value=time)
-            else: 
-                if labor_type_id in labor_info.keys():
-                    num_labor = labor_info[labor_type_id]
-                    labor_time_sheet.cell(row=idx+2, column=seq+1, value=num_labor)
-                else:
-                    labor_time_sheet.cell(row=idx+2, column=seq+1, value=0)
-        idx += 1
-    
-    wb.save(result_path)
-    wb.close()
+    wb.save(result_path+".xlsx")
+    wb.close()    
     return
 
 
@@ -125,3 +168,63 @@ def create_gantt(data_path, out_dir, schedule: Schedule):
     pyplt(fig, filename=out_dir+"workpackage_chart.html", auto_open=False)    
     pyplt(fig_3, filename=out_dir+"section_chart.html", auto_open=False)  
     return
+
+
+def get_labor_list(json_data):
+    labor_id_list = []
+    for data in json_data.values():        
+        for labor_id, num_labor in data.items():
+            if labor_id not in labor_id_list:
+                labor_id_list.append(labor_id)
+    return labor_id_list
+    
+
+def create_new_labor_dict(json_data):    
+    labor_id_list = get_labor_list(json_data)
+    new_json_data = {}
+    for labor_id in labor_id_list:
+        new_json_data[labor_id] = []
+
+    new_json_data["time"] = []
+
+    t = 0
+    for time, data in json_data.items():
+        new_json_data["time"].append(time)
+        for labor_id in labor_id_list:
+            if labor_id == "time":
+                continue
+            if labor_id in data.keys():
+                num_labor = data[labor_id]
+                new_json_data[labor_id].append(num_labor)
+            else:
+                new_json_data[labor_id].append(0)            
+        t += 1
+    
+    return new_json_data
+
+def create_labor_chart(data_path, out_dir):
+    with open(data_path, 'r', encoding="utf-8") as f:
+        json_data = json.load(f)
+
+    labor_id_list = get_labor_list(json_data)
+    new_data = create_new_labor_dict(json_data)
+    
+    
+    # new_data = pd.DataFrame(new_data)
+    
+   
+    fig = go.Figure()
+    for labor_id in labor_id_list:
+        fig.add_trace(go.Scatter(x=new_data["time"], y=new_data[labor_id],
+                            mode='lines', # Line plot만 그리기
+                            name=labor_id))
+        # fig.add_trace(go.Bar(
+        #     x=new_data["time"],
+        #     y=new_data[labor_id],
+        #     name=labor_id
+        # ))
+    pyplt = py.offline.plot
+    pyplt(fig, filename=out_dir+"labor_chart.html", auto_open=False)  
+
+    return
+        
